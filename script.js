a51f6c280856518df93677e6d0b256ebe444194f
vOganezova <victoriaoganezova1@gmail.com>
2022-11-27 16:02:59 +0400

    basic setup

---
+++ script.js
@@ -0,0 +1,5 @@
+const arrowLeft = document.getElementById("arrow-left");
+const arrowRight = document.getElementById("arrow-right");
+const sliderCOntent = document.getElementById("slider-content");
+let sliderIndex = 0;
+
